685324b7d17adb93904c056156cd686fd083ffad
wolfchase <rvallada@ucsc.edu>
2016-11-10 23:55:21 -0800

    Initial Project db table

--- models/tables.py
+++ models/tables.py
@@ -9,18 +9,16 @@
 
 import datetime
 
-db.define_table('post',
-                Field('user_email', default=auth.user.email if auth.user_id else None),
-                Field('post_content', 'text'),
-                Field('created_on', 'datetime', default=datetime.datetime.utcnow()),
-                Field('updated_on', 'datetime', update=datetime.datetime.utcnow()),
+db.define_table('project',
+                Field('project_name', 'text'),
+                Field('phase', 'text'),
+                Field('version', 'text'),
+                Field('created_on', 'datetime', default=datetime.datetime.utcnow())
                 )
 
-# I don't want to display the user email by default in all forms.
-db.post.user_email.readable = db.post.user_email.writable = False
-db.post.post_content.requires = IS_NOT_EMPTY()
-db.post.created_on.readable = db.post.created_on.writable = False
-db.post.updated_on.readable = db.post.updated_on.writable = False
+db.project.project_name.requires = IS_NOT_EMPTY()
+db.project.phase.requires = IS_NOT_EMPTY()
+db.project.version.requires = IS_NOT_EMPTY()
 
 # after defining tables, uncomment below to enable auditing
 # auth.enable_record_versioning(db)
